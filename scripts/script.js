95bcc5849cdfeceb2edc6e2c09cb035fb85c0110
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-02-01 15:19:13 +0000

    THE REMOVE BUTTON IS WORKING!!!

--- scripts/script.js
+++ scripts/script.js
@@ -5,17 +5,19 @@ const SEARCH_BUTTON = $('.searchButton');
 const NUTRI_ADD_BUTTON = $('.nutriAdd');
 const MEAL_ADD_BUTTON = $('.mealAdd');
 const DAY_ADD_BUTTON = $('.dayAdd');
-// const REMOVE_BUTTON = $('<button>');
-// REMOVE_BUTTON.text("Remove");
+const REMOVE_BUTTON = $('<button>').addClass("remove");
+REMOVE_BUTTON.text("Remove");
 const MEAL_TOTAL_BUTTON = $('.mealTotalBtn');
 const DAY_TOTAL_BUTTON = $('.dayTotalBtn');
+const WEEK_TOTAL_BUTTON = $('.weekTotalBtn');
 const NUTRI_DIV = $('.nutri');
-const ITEM_DIV = $('<div>').addClass('ITEM_DIV');
+const ITEM_DIV = $('<div>');
 const MEAL_DIV = $('.meal');
 const DAILY_DIV = $('.daily');
 const WEEKLY_DIV = $('.weekly');
 const MEAL_TOTAL_DIV = $('.mealTotal');
 const DAILY_TOTAL_DIV = $('.dailyTotal');
+const WEEKLY_TOTAL_DIV = $('.weeklyTotal');
 let FOOD_ITEMS = JSON.parse(localStorage.getItem('food')) || [];
 let MEAL_FOODS_ARR = [];
 let DAILY_FOODS_ARR = [];
@@ -29,7 +31,7 @@ function getRandomArrIndex(array) {
 }
 
 function copyAppend(cloneItem, targetDiv) {
-    targetDiv.append(cloneItem.clone(true));
+    targetDiv.append(cloneItem.clone(true, true));
 }
 
 function getTotal(DIV) {
@@ -51,27 +53,31 @@ function renderCurrentNutri(searchItem) {
     ITEM_DIV.empty();
     let CURRENT_FOODS = [];
     CURRENT_FOODS.push(currentFoodObj);
-    // ITEM_DIV.append(REMOVE_BUTTON);
+    ITEM_DIV.append(REMOVE_BUTTON);
     CURRENT_FOODS.forEach(foodItem => {
-        let foodNameDiv = $("<p>").text(searchItem);
-        ITEM_DIV.append(foodNameDiv);
-        let caloriesDiv = $("<p>").text(" - Calories (kcal): " + foodItem.calories);
-        ITEM_DIV.append(caloriesDiv);
-        let proteinDiv = $("<p>").text(" - Protein (g): " + foodItem.protein);
-        proteinDiv.attr("ID", "protein");
-        ITEM_DIV.append(proteinDiv);
-        let fatDiv = $("<p>").text(" - Fat (g): " + foodItem.fat);
-        fatDiv.attr("ID", "fats");
-        ITEM_DIV.append(fatDiv);
-        let carbDiv = $("<p>").text(" - Carbs (g): " + foodItem.carb);
-        carbDiv.attr("ID", "carbs");
-        ITEM_DIV.append(carbDiv);
-        NUTRI_DIV.append(ITEM_DIV);
-    })
+            let foodNameDiv = $("<p>").text(searchItem);
+            ITEM_DIV.append(foodNameDiv);
+            let caloriesDiv = $("<p>").text(" - Calories (kcal): " + foodItem.calories);
+            ITEM_DIV.append(caloriesDiv);
+            let proteinDiv = $("<p>").text(" - Protein (g): " + foodItem.protein);
+            proteinDiv.attr("ID", "protein");
+            ITEM_DIV.append(proteinDiv);
+            let fatDiv = $("<p>").text(" - Fat (g): " + foodItem.fat);
+            fatDiv.attr("ID", "fats");
+            ITEM_DIV.append(fatDiv);
+            let carbDiv = $("<p>").text(" - Carbs (g): " + foodItem.carb);
+            carbDiv.attr("ID", "carbs");
+            ITEM_DIV.append(carbDiv);
+            NUTRI_DIV.append(ITEM_DIV);
+        })
+        // renderDrop(searchItem);
 }
 
 // THIS IS A BETA FUNCTION. Currently when the information displays it always displays the entirety of the nutritional information. With this we could display the nutrition for the nutritional section
 // and for the rest of the code we could have it as a drop down so it just looks a little nicer. 
+
+// $('.collapsible').collapsible();
+
 // function renderDrop(searchItem) {
 //     let collapsible = $("<ul>").addClass("collapsible");
 //     let listItem = $("<li>");
@@ -90,11 +96,6 @@ function renderCurrentNutri(searchItem) {
 //     collapsible.append(body);
 //     NUTRI_DIV.append(collapsible);
 // }
-
-// THIS IS TO ALLOW COLLAPSIBLE ITEMS TO WORK
-// $(document).ready(function() {
-//     $('.collapsible').collapsible();
-// });
 
 
 // <--- MAIN --->
@@ -167,7 +168,7 @@ MEAL_ADD_BUTTON.on('click', function() {
 DAY_ADD_BUTTON.on('click', function() {
     copyAppend(DAILY_DIV, WEEKLY_DIV)
     WEEKLY_FOODS_ARR.push(DAILY_FOODS_ARR);
-});
+})
 
 MEAL_TOTAL_BUTTON.on('click', function() {
     getTotal(MEAL_TOTAL_DIV);
@@ -177,6 +178,14 @@ DAY_TOTAL_BUTTON.on('click', function() {
     getTotal(DAILY_TOTAL_DIV);
 })
 
-// REMOVE_BUTTON.on('click', function() {
-//     $(this).parent().remove();
-// });
+WEEK_TOTAL_BUTTON.on('click', function() {
+    getTotal(WEEKLY_TOTAL_DIV);
+})
+
+REMOVE_BUTTON.on('click', function() {
+    $(this).parent().remove();
+})
+
+$("div").on('click', ".remove", function() {
+    $(this).parent().remove();
+})
