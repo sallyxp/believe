606bffbfacf24eb79aa946861ca9e2b4e0f78980
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-02-03 21:14:00 +0000

    merge fixed

---
+++ scripts/script_goals.js
@@ -0,0 +1,54 @@
+var goalInput = document.querySelector("#my_goal");
+var addBtn = document.querySelector("#add-btn");
+var goalList = document.querySelector("#goal-list");
+
+var goals = [];
+localStorage.setItem("goals", JSON.stringify(goals));
+var storedGoals = JSON.parse(localStorage.getItem("goals"));
+
+renderGoals();
+
+function renderGoals() {
+    goalList.innerHTML = "";
+
+    for (var i = 0; i < goals.length; i++) {
+        var goal = goals[i];
+
+        var li = document.createElement("li");
+        li.innerText = goal;
+        console.log(li);
+        li.setAttribute("data-index", i);
+        var achieveBtn = document.createElement("button");
+        achieveBtn.textContent = "Achieved";
+        li.append(achieveBtn);
+        goalList.append(li);
+    }
+}
+
+addBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    console.log(goalInput.value);
+    var goalText = goalInput.value.trim();
+
+    if (goalText === "") {
+        return;
+    }
+
+    goals.push(goalText);
+    goalInput.value;
+
+    renderGoals();
+})
+
+goalList.addEventListener("click", function (event) {
+    var element = event.target;
+
+    if (element.matches("button") === true) {
+        var index = element.parentElement.getAttribute("data-index");
+        goals.splice(index, 1);
+
+
+        renderGoals();
+    }
+})
